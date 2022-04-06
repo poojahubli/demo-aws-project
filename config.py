1a154e6299803a0643a5c24f55f55fc3f5d50ffd
poojahubli <103031050+poojahubli@users.noreply.github.com>
2022-04-06 20:34:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = pj2.c0cfntdl1bby.us-east-1.rds.amazonaws.com
+customhost = "pj2.c0cfntdl1bby.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin1234"
 customdb = "pj2"
