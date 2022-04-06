32ddc5048813c66a59e90ebd5a23f7136db609d5
poojahubli <103031050+poojahubli@users.noreply.github.com>
2022-04-06 15:44:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = project2.c0cfntdl1bby.us-east-1.rds.amazonaws.com
+customhost = project24.c0cfntdl1bby.us-east-1.rds.amazonaws.com
 customuser = "admin"
 custompass = "admin123"
-customdb = "project2"
-custombucket = "project24"
-customregion = "us-east-1"
+customdb = "project24"
+custombucket = "project24aws2"
+customregion = " us-east-1"
